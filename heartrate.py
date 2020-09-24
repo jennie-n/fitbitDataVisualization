7f8eb7837143678860017ad46bbf5f6f388e9987
Kenneth Nguyen <n.kennethcs@gmail.com>
2020-09-23 22:15:11 -0700

    change heart rate data to fetch resting heart rate for specific time interval

--- heartrate.py
+++ heartrate.py
@@ -4,16 +4,19 @@ from pprint import pprint
 
 # HEART RATE ON THIS DAY
 def get_heartrate(user_id, access_token):
-    heart_rate_request = requests.get('https://api.fitbit.com/1/user/' + user_id + '/activities/heart/date/2020-09-01/1d/1min/time/09:00/22:00.json',
+    heart_rate_request = requests.get('https://api.fitbit.com/1/user/' + user_id + '/activities/heart/date/2020-08-16/2020-08-31.json',
                                      headers={'Authorization': 'Bearer ' + access_token})
     # print(heart_rate_request.status_code)
 
-    # pprint(heart_rate_request.json()['activities-heart-intraday']['dataset'])
-    data = heart_rate_request.json()['activities-heart-intraday']['dataset']
+    # pprint(heart_rate_request.json()['activities-heart'])
+    data = heart_rate_request.json()['activities-heart']
+    # d = data[0]
+    # print(d['dateTime'])
+    # print(d['value']['restingHeartRate'])
 
     # extract heart rate values to new csv file
     with open("./csv/heartrate.csv", "w", newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         for line in data:
-            # print(line['value'])
-            writer.writerow(line.values())
+            # print(line['value']['restingHeartRate'])
+            writer.writerow([line['dateTime'], line['value']['restingHeartRate']])
